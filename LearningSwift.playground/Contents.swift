4c5f533f4a39166fcf5fe3388fc90222caab0053
vashishta <adi.vashisht@agilex.com>
2016-04-04 11:40:49 -0400

    Adding playground

---
+++ LearningSwift.playground/Contents.swift
@@ -0,0 +1,24 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+
+var str = "Hello, playground"
+
+var numberOfStopLights = 4
+numberOfStopLights+=2
+
+print(numberOfStopLights)
+
+let stopLights:Int16 = 4
+print(stopLights)
+
+// MARK
+
+let townName:String = "Chantilly"
+var population:Int16 = 5422
+var unemploymentLevel:Double
+
+let townDescription = "\(townName) has a population of \(population) and \(stopLights)"
+
+
